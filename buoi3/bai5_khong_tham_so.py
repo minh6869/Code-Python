4843f314c53498eaa8db8d6c4f71fffac40fd38a
ZhiiShuu <kali@kali.kali>
2023-07-05 12:41:57 +0700

    điều chỉnh bài 5 không tham số

--- buoi3/bai5_khong_tham_so.py
+++ buoi3/bai5_khong_tham_so.py
@@ -13,14 +13,15 @@ def tim_uoc_boi_chung():
         if num <= 0:
             break
         numbers.append(num)
-
-    uscln = numbers[0]
-    bscnn = numbers[0]
-    for i in range(1, len(numbers)):
-        uscln = uoc_so_chung_lon_nhat(uscln, numbers[i])
-        bscnn = boi_so_chung_nho_nhat(bscnn, numbers[i])
-
-    print("Ước số chung lớn nhất:", uscln)
-    print("Bội số chung nhỏ nhất:", bscnn)
+    if len(numbers) != 0:
+        uscln = numbers[0]
+        bscnn = numbers[0]
+        for i in range(1, len(numbers)):
+            uscln = uoc_so_chung_lon_nhat(uscln, numbers[i])
+            bscnn = boi_so_chung_nho_nhat(bscnn, numbers[i])
+        print("Ước số chung lớn nhất:", uscln)
+        print("Bội số chung nhỏ nhất:", bscnn)
+    else:
+        print("Day nhap khong hop le")
 
 tim_uoc_boi_chung()
